81df405711bd4077b0e6711ff62f0635b4e53113
Jiten <jpalaparthi@gmail.com>
2023-09-15 11:17:48 +0530

    add more channels

--- 15-generator-channel/main.go
+++ 15-generator-channel/main.go
@@ -1,17 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	sig1 := make(chan struct{})
-	sig2 := make(chan struct{})
+	//sig1 := make(chan struct{})
+	//sig2 := make(chan struct{})
 
 	ch1 := send(20, "sender-1")
 	ch2 := send(20, "sender-2")
+	// ch3 := send(20, "sender-3")
 
-	go receive(ch1, sig1, "receiver-1")
-	go receive(ch2, sig2, "receiver-2")
-
+	// go receive(ch1, sig1, "receiver-1")
+	// go receive(ch2, sig2, "receiver-2")
+	// <-sig1
+	// <-sig2
+	// <-receive2(ch3, "receiver-3") // This is not a async operation
+	sig1, sig2 := receive3(ch1, ch2)
+	// sig1, sig2 := receive4(ch1, ch2)
 	<-sig1
 	<-sig2
 }
@@ -33,5 +41,76 @@ func receive(ch <-chan string, signal chan<- struct{}, routine string) {
 	for sq := range ch {
 		fmt.Println("received by ", routine, sq)
 	}
-	signal <- struct{}{}
+	signal <- struct{}{} // instead of it can also use close(singal)
 }
+
+func receive2(ch <-chan string, routine string) chan struct{} {
+	signal := make(chan struct{}, 1)
+	// for sq := range ch {
+	// 	fmt.Println("received by ", routine, sq)
+	// }
+
+	go func() {
+		defer fmt.Println("This go routine is exiting")
+		//outer:
+		for {
+			select {
+			case sq, ok := <-ch:
+				if !ok {
+					close(signal) // close channel also sends a signal
+					runtime.Goexit()
+					//break outer
+				} else {
+					fmt.Println("received by ", routine, sq)
+				}
+			}
+		}
+	}()
+	return signal
+}
+
+func receive3(ch1, ch2 chan string) (sig1 chan struct{}, sig2 chan struct{}) {
+	sig1 = receive2(ch1, "receiver1 ch1")
+	sig2 = receive2(ch2, "receiver1 ch1")
+	return sig1, sig2
+}
+
+// func receive4(ch1, ch2 chan string) (sig1 chan struct{}, sig2 chan struct{}) {
+// 	signal1 := make(chan struct{}, 1)
+// 	signal2 := make(chan struct{}, 1)
+// 	go func() {
+// 		counter := 0
+// 		for {
+// 			select {
+// 			case sq, ok := <-ch1:
+// 				if !ok {
+// 					counter++
+// 					//close(signal1) // close channel also sends a signal
+// 					//runtime.Goexit()
+// 					//break outer
+// 					//signal1 <- struct{}{}
+// 				} else {
+// 					fmt.Println("received by ch1->", sq)
+// 				}
+
+// 			case sq, ok := <-ch2:
+// 				if !ok {
+// 					counter++
+// 					//close(signal2) // close channel also sends a signal
+// 					//runtime.Goexit()
+// 					//break outer
+// 					//signal2 <- struct{}{}
+// 				} else {
+// 					fmt.Println("received by ch2->", sq)
+// 				}
+// 			default:
+// 				if counter == 2 {
+// 					runtime.Goexit()
+// 				}
+// 			}
+// 		}
+// 	}()
+
+// 	return signal1, signal2
+
+// }
--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 - select 
 
 - only sender can close a channel 
+- upon closing a channel , it not only closes the channel but also it sends a value.It sends the default value of the type (according to the type inference of go)
 - range loop on a channel , it receives the value until the channel is closed
 - when multiple go routines are trying to fetch values from a channel (buffered), the order is not guaranteed 
